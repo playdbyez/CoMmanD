032cfb09248ec0d23e30f1f9e0a72a90f09b67a7
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-09-23 13:17:25 +0100

    Add files via upload

---
+++ Bashd/Exit_history/exitr.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+
+echo '' > ~/.local/share/recently-used.xbel
+echo '[+]File:recently-used'
+sleep .4
+
+echo '' > ~/.bash_history
+echo '[+]File:bash_history'
+sleep .4
+echo '\n[+]Clear'
+
+
